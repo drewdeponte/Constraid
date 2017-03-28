10c745334cb9ce72cd5a19f47bd0d48767e4579b
Andrew De Ponte <cyphactor@gmail.com>
2017-03-28 09:30:38 -0700

    Changed formatting to meet < 80 columns
    Why you made the change:
    
    I did this so that it meets our style guide standards.

--- Constraid/CenterWithinEdges.swift
+++ Constraid/CenterWithinEdges.swift
@@ -6,33 +6,45 @@
 
 extension ConstraidView {
     @discardableResult
-    open func center(verticallyWithin item: Any?, constant: CGFloat = 0.0, multiplier: CGFloat = 1.0, priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired) -> ConstraidConstraintCollection {
+    open func center(verticallyWithin item: Any?,
+        constant: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
 
         self.translatesAutoresizingMaskIntoConstraints = false
         let collection = ConstraidConstraintCollection([
-            NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal,
-                               toItem: item, attribute: .centerY, multiplier: multiplier,
-                               constant: constant, priority: priority)
+            NSLayoutConstraint(item: self, attribute: .centerY,
+                relatedBy: .equal, toItem: item, attribute: .centerY,
+                multiplier: multiplier, constant: constant, priority: priority)
             ])
         collection.activate()
         return collection
     }
 
     @discardableResult
-    open func center(horizontallyWithin item: Any?, constant: CGFloat = 0.0, multiplier: CGFloat = 1.0, priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired) -> ConstraidConstraintCollection {
+    open func center(horizontallyWithin item: Any?,
+        constant: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
 
         self.translatesAutoresizingMaskIntoConstraints = false
         let collection = ConstraidConstraintCollection([
-            NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal,
-                               toItem: item, attribute: .centerX, multiplier: multiplier,
-                               constant: constant, priority: priority)
+            NSLayoutConstraint(item: self, attribute: .centerX,
+                relatedBy: .equal, toItem: item, attribute: .centerX,
+                multiplier: multiplier, constant: constant, priority: priority)
             ])
         collection.activate()
         return collection
     }
 
     @discardableResult
-    open func center(within item: Any?, constant: CGFloat = 0.0, multiplier: CGFloat = 1.0, priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired) -> ConstraidConstraintCollection {
+    open func center(within item: Any?,
+        constant: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
 
         let constraints = center(horizontallyWithin: item, constant: constant,
                             multiplier: multiplier, priority: priority) +
--- Constraid/CenterWithinMargins.swift
+++ Constraid/CenterWithinMargins.swift
@@ -4,36 +4,52 @@
 import UIKit
 
 extension ConstraidView {
-    open func center(verticallyWithinMarginsOf item: Any?, constant: CGFloat = 0.0, multiplier: CGFloat = 1.0, priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired) -> ConstraidConstraintCollection {
+    open func center(verticallyWithinMarginsOf item: Any?,
+        constant: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
 
         self.translatesAutoresizingMaskIntoConstraints = false
         let collection = ConstraidConstraintCollection([
-            NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal,
-                               toItem: item, attribute: .centerYWithinMargins,
-                               multiplier: multiplier, constant: constant, priority: priority)
+            NSLayoutConstraint(item: self, attribute: .centerY,
+                relatedBy: .equal, toItem: item,
+                attribute: .centerYWithinMargins, multiplier: multiplier,
+                constant: constant, priority: priority)
             ])
         collection.activate()
         return collection
     }
 
-    open func center(horizontallyWithinMarginsOf item: Any?, constant: CGFloat = 0.0, multiplier: CGFloat = 1.0, priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired) -> ConstraidConstraintCollection{
+    open func center(horizontallyWithinMarginsOf item: Any?,
+        constant: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
 
         self.translatesAutoresizingMaskIntoConstraints = false
         let collection = ConstraidConstraintCollection([
-            NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal,
-                               toItem: item, attribute: .centerXWithinMargins,
-                               multiplier: multiplier, constant: constant, priority: priority)
+            NSLayoutConstraint(item: self, attribute: .centerX,
+                relatedBy: .equal, toItem: item,
+                attribute: .centerXWithinMargins, multiplier: multiplier,
+                constant: constant, priority: priority)
             ])
         collection.activate()
         return collection
     }
 
-    open func center(withinMarginsOf item: Any?, constant: CGFloat = 0.0, multiplier: CGFloat = 1.0, priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired) -> ConstraidConstraintCollection {
+    open func center(withinMarginsOf item: Any?,
+        constant: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
 
-        let constraints = center(horizontallyWithinMarginsOf: item, constant: constant,
-                            multiplier: multiplier, priority: priority) +
-                          center(verticallyWithinMarginsOf: item, constant: constant,
-                            multiplier: multiplier, priority: priority)
+        let constraints = center(horizontallyWithinMarginsOf: item,
+                            constant: constant, multiplier: multiplier,
+                            priority: priority) +
+                          center(verticallyWithinMarginsOf: item,
+                            constant: constant, multiplier: multiplier,
+                            priority: priority)
         constraints.activate()
         return constraints
     }
