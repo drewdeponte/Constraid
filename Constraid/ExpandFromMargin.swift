94899079cb1d8fb903eace3696d5f9baafe011c7
Andrew De Ponte <cyphactor@gmail.com>
2017-04-24 22:11:26 -0700

    Deprecated expand(fromMarginsOf:, constant:) methods
    Why you made the change:
    
    I did this because I have now provided the new more discoverable API
    naming with the expand(fromMarginsOf:, offsetBy:) methods. The intent of
    this change is to make the API more understandable during the act of
    coding without the need to reference documentation or perform a bunch of
    trial and error iterations to understand what giving `constant` a
    positive value vs a negative value would do.

--- Constraid/ExpandFromMargin.swift
+++ Constraid/ExpandFromMargin.swift
@@ -6,6 +6,130 @@ import UIKit
 extension ConstraidView {
     @discardableResult
     open func expand(fromLeadingMarginOf item: Any?,
+        offsetBy offset: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
+
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let collection = ConstraidConstraintCollection([
+            NSLayoutConstraint(item: self, attribute: .leading,
+                relatedBy: .greaterThanOrEqual, toItem: item,
+                attribute: .leadingMargin, multiplier: multiplier,
+                constant: offset, priority: priority)
+            ])
+        collection.activate()
+        return collection
+    }
+
+    @discardableResult
+    open func expand(fromTrailingMarginOf item: Any?,
+        offsetBy offset: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
+
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let collection = ConstraidConstraintCollection([
+            NSLayoutConstraint(item: self, attribute: .trailing,
+                relatedBy: .greaterThanOrEqual, toItem: item,
+                attribute: .trailingMargin, multiplier: multiplier,
+                constant: (offset * -1), priority: priority)
+            ])
+        collection.activate()
+        return collection
+    }
+
+    @discardableResult
+    open func expand(fromTopMarginOf item: Any?,
+        offsetBy offset: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
+
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let collection = ConstraidConstraintCollection([
+            NSLayoutConstraint(item: self, attribute: .top,
+                relatedBy: .greaterThanOrEqual, toItem: item,
+                attribute: .topMargin, multiplier: multiplier,
+                constant: offset, priority: priority)
+            ])
+        collection.activate()
+        return collection
+    }
+
+    @discardableResult
+    open func expand(fromBottomMarginOf item: Any?,
+        offsetBy offset: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
+
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let collection = ConstraidConstraintCollection([
+            NSLayoutConstraint(item: self, attribute: .bottom,
+                relatedBy: .greaterThanOrEqual, toItem: item,
+                attribute: .bottomMargin, multiplier: multiplier,
+                constant: (offset * -1), priority: priority)
+            ])
+        collection.activate()
+        return collection
+    }
+
+    @discardableResult
+    open func expand(fromHorizontalMarginsOf item: Any?,
+        offsetBy offset: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
+
+        let collection = expand(fromTopMarginOf: item, offsetBy: offset,
+                                multiplier: multiplier, priority: priority) +
+                         expand(fromBottomMarginOf: item, offsetBy: offset,
+                                multiplier: multiplier, priority: priority)
+        collection.activate()
+        return collection
+    }
+
+    @discardableResult
+    open func expand(fromVerticalMarginsOf item: Any?,
+        offsetBy offset: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
+
+        let collection = expand(fromLeadingMarginOf: item, offsetBy: offset,
+                                multiplier: multiplier, priority: priority) +
+                         expand(fromTrailingMarginOf: item, offsetBy: offset,
+                                multiplier: multiplier, priority: priority)
+        collection.activate()
+        return collection
+    }
+
+    @discardableResult
+    open func expand(fromMarginsOf item: Any?,
+        offsetBy offset: CGFloat = 0.0,
+        multiplier: CGFloat = 1.0,
+        priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
+        ) -> ConstraidConstraintCollection {
+
+        let collection = expand(fromTopMarginOf: item, offsetBy: offset,
+                                multiplier: multiplier, priority: priority) +
+                         expand(fromBottomMarginOf: item, offsetBy: offset,
+                                multiplier: multiplier, priority: priority) +
+                         expand(fromLeadingMarginOf: item, offsetBy: offset,
+                                multiplier: multiplier, priority: priority) +
+                         expand(fromTrailingMarginOf: item, offsetBy: offset,
+                                multiplier: multiplier, priority: priority)
+        collection.activate()
+        return collection
+    }
+
+    // MARK: - Deprecated
+
+    @discardableResult
+    @available(*, deprecated, message: "use expand(fromLeadigMarginOf: , offsetBy: ...)")
+    open func expand(fromLeadingMarginOf item: Any?,
         constant: CGFloat = 0.0,
         multiplier: CGFloat = 1.0,
         priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired
@@ -23,6 +147,7 @@ extension ConstraidView {
     }
 
     @discardableResult
+    @available(*, deprecated, message: "use expand(fromTrailingMarginOf: , offsetBy: ...)")
     open func expand(fromTrailingMarginOf item: Any?,
         constant: CGFloat = 0.0,
         multiplier: CGFloat = 1.0,
@@ -41,6 +166,7 @@ extension ConstraidView {
     }
 
     @discardableResult
+    @available(*, deprecated, message: "use expand(fromTopMarginOf: , offsetBy: ...)")
     open func expand(fromTopMarginOf item: Any?,
         constant: CGFloat = 0.0,
         multiplier: CGFloat = 1.0,
@@ -59,6 +185,7 @@ extension ConstraidView {
     }
 
     @discardableResult
+    @available(*, deprecated, message: "use expand(fromBottomMarginOf: , offsetBy: ...)")
     open func expand(fromBottomMarginOf item: Any?,
         constant: CGFloat = 0.0,
         multiplier: CGFloat = 1.0,
@@ -77,6 +204,7 @@ extension ConstraidView {
     }
 
     @discardableResult
+    @available(*, deprecated, message: "use expand(fromHorizontalMarginsOf: , offsetBy: ...)")
     open func expand(fromHorizontalMarginsOf item: Any?,
         constant: CGFloat = 0.0,
         multiplier: CGFloat = 1.0,
@@ -92,6 +220,7 @@ extension ConstraidView {
     }
 
     @discardableResult
+    @available(*, deprecated, message: "use expand(fromVerticalMarginsOf: , offsetBy: ...)")
     open func expand(fromVerticalMarginsOf item: Any?,
         constant: CGFloat = 0.0,
         multiplier: CGFloat = 1.0,
@@ -107,6 +236,7 @@ extension ConstraidView {
     }
 
     @discardableResult
+    @available(*, deprecated, message: "use expand(fromMarginsOf: , offsetBy: ...)")
     open func expand(fromMarginsOf item: Any?,
         constant: CGFloat = 0.0,
         multiplier: CGFloat = 1.0,
