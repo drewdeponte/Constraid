87f313cd60b07c094f8931defcf5f7134d1f17ed
Andrew De Ponte <cyphactor@gmail.com>
2017-04-25 18:57:11 -0700

    Document ConstraidView
    Why you made the change:
    
    I did this so that people would understand why I created a type alias
    called ConstraidView that maps to either UIView or NSView depending on
    the platform it is being built against.

--- Constraid/View.swift
+++ Constraid/View.swift
@@ -1,7 +1,13 @@
 #if os(iOS)
     import UIKit
+    /**
+        Type alias for UIVIew and NSView to make supporting both easier
+    */
     public typealias ConstraidView = UIView
 #else
     import AppKit
+    /**
+        Type alias for UIVIew and NSView to make supporting both easier
+    */
     public typealias ConstraidView = NSView
 #endif
