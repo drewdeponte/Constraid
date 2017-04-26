5ae96189da3c328fae8eac76cb5aa4d99eb5d271
Andrew De Ponte <cyphactor@gmail.com>
2017-04-25 19:09:08 -0700

    Document Constraint collection extension
    Why you made the change:
    
    I did this so that people know what the methods for activation and
    deactivation do on the collection of NSLayoutConstraints.

--- Constraid/ConstraintCollection.swift
+++ Constraid/ConstraintCollection.swift
@@ -4,13 +4,23 @@
     import AppKit
 #endif
 
+/**
+    Type alias for [NSLayoutConstraint] in case Apple makes constraints not
+    cross-platform like they have with other parts of the auto-layout API
+*/
 public typealias ConstraidConstraintCollection = [NSLayoutConstraint]
 
 public extension Collection where Iterator.Element == NSLayoutConstraint {
+    /**
+        Activate the collection of constraints
+    */
     public func activate() {
         NSLayoutConstraint.activate(self as! [NSLayoutConstraint])
     }
 
+    /**
+        Deactivate the collection of constraints
+    */
     public func deactivate() {
         NSLayoutConstraint.deactivate(self as! [NSLayoutConstraint])
     }
