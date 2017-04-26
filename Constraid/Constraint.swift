c364c64065b5573b018faca1aeb153e5ee44f1ff
Andrew De Ponte <cyphactor@gmail.com>
2017-04-25 23:21:22 -0700

    Document NSLayoutConstraint priority constructor
    Why you made the change:
    
    I did this so that people would be able to quickly find out how to use
    the priority constructor that we provide for the NSLayoutConstraint.

--- Constraid/Constraint.swift
+++ Constraid/Constraint.swift
@@ -5,13 +5,37 @@
 #endif
 
 public extension NSLayoutConstraint {
-    public convenience init(item: Any, attribute attr1: NSLayoutAttribute,
-                            relatedBy: NSLayoutRelation, toItem: Any?,
-                            attribute attr2: NSLayoutAttribute, multiplier: CGFloat,
-                            constant: CGFloat, priority: ConstraidLayoutPriority) {
+    /**
+        Construct instance of NSLayoutConstraint with priority set
 
-        self.init(item: item, attribute: attr1, relatedBy: relatedBy, toItem: toItem,
-                  attribute: attr2, multiplier: multiplier, constant: constant)
+        See UIKit NSLayoutConstraint for further details
+     
+        - parameter item: The `item` you want to constrain
+        - parameter attr1: The attribute of `item` to constrain
+        - parameter relatedBy: relation ship to use to constrain `item`'s
+            `attr1` to `toItem`'s `attr2`
+        - parameter toItem: The item you want to constrain `item` to
+        - parameter attr2: The attribute of the `toItem` to constrain
+        - parameter mutiplier: The ratio altering the constraint relative to
+            toItem's attr2 in the constraint equation
+        - parameter constant: The constant in the constraint equation
+        - parameter priority: The priority this constraint uses when being
+            evaluated against other constraints
+
+        - returns: the constructed NSLayoutConstraint
+    */
+    public convenience init(item: Any,
+        attribute attr1: NSLayoutAttribute,
+        relatedBy: NSLayoutRelation,
+        toItem: Any?,
+        attribute attr2: NSLayoutAttribute,
+        multiplier: CGFloat,
+        constant: CGFloat,
+        priority: ConstraidLayoutPriority) {
+
+        self.init(item: item, attribute: attr1, relatedBy: relatedBy,
+            toItem: toItem, attribute: attr2, multiplier: multiplier,
+            constant: constant)
         self.priority = priority
     }
 }
