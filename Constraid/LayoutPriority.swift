9f43e87efdf7bb2fa4636f7774a5546e68d8efdf
Andrew De Ponte <cyphactor@gmail.com>
2017-04-25 19:00:04 -0700

    Document ConstraidLayoutPriority
    Why you made the change:
    
    I did this so that people would know why the ConstraidLayoutPriority
    type alias exists and what the intent of it is.

--- Constraid/LayoutPriority.swift
+++ Constraid/LayoutPriority.swift
@@ -5,12 +5,20 @@
 #endif
 
 #if os(iOS)
+    /**
+        Type alias for UILayoutPriority and NSLayoutPriority to make supporting
+        both iOS and Mac OS easier
+    */
     public typealias ConstraidLayoutPriority = UILayoutPriority
 let ConstraidLayoutPriorityRequired: ConstraidLayoutPriority = UILayoutPriorityRequired
 let ConstraidLayoutPriorityDefaultHigh: ConstraidLayoutPriority = UILayoutPriorityDefaultHigh
 let ConstraidLayoutPriorityDefaultLow: ConstraidLayoutPriority = UILayoutPriorityDefaultLow
 let ConstraidLayoutPriorityFittingSizeLevel: ConstraidLayoutPriority = UILayoutPriorityFittingSizeLevel
 #else
+    /**
+        Type alias for UILayoutPriority and NSLayoutPriority to make supporting
+        both iOS and Mac OS easier
+    */
     public typealias ConstraidLayoutPriority = NSLayoutPriority
 let ConstraidLayoutPriorityRequired: ConstraidLayoutPriority = NSLayoutPriorityRequired
 let ConstraidLayoutPriorityDefaultHigh: ConstraidLayoutPriority = NSLayoutPriorityDefaultHigh
