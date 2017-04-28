92365abe2fffbbb9b09d19355a3e01cbc049b273
Andrew De Ponte <cyphactor@gmail.com>
2017-04-27 19:49:39 -0700

    Document Manage Intrinsic Size Relations
    Why you made the change:
    
    I did this so that people would understand better what these methods do.

--- Constraid/ManageIntrinsicSizeRelations.swift
+++ Constraid/ManageIntrinsicSizeRelations.swift
@@ -5,16 +5,28 @@
 #endif
 
 extension ConstraidView {
+    /**
+        Sets height content hugging and compression resistance priority to 1000
+        (required)
+    */
     open func keepIntrinsicHeight(priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired) {
         self.setContentCompressionResistancePriority(priority, for: .vertical)
         self.setContentHuggingPriority(priority, for: .vertical)
     }
 
+    /**
+        Sets width content hugging and compression resistance priority to 1000
+        (required)
+    */
     open func keepIntrinsicWidth(priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired) {
         self.setContentCompressionResistancePriority(priority, for: .horizontal)
         self.setContentHuggingPriority(priority, for: .horizontal)
     }
 
+    /**
+        Sets height & width content hugging and compression resistance priority to 1000
+        (required)
+    */
     open func keepIntrinsicSize(priority: ConstraidLayoutPriority = ConstraidLayoutPriorityRequired) {
         keepIntrinsicHeight(priority: priority)
         keepIntrinsicWidth(priority: priority)
