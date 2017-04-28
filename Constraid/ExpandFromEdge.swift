f3e1294764ca51d5477d0263cd366067a7e28a5a
Andrew De Ponte <cyphactor@gmail.com>
2017-04-27 20:08:07 -0700

    Document ExpandFromEdge methods
    Why you made the change:
    
    I did this so that people would better understand in detail what these
    methods are doing.

--- Constraid/ExpandFromEdge.swift
+++ Constraid/ExpandFromEdge.swift
@@ -5,6 +5,20 @@
 #endif
 
 extension ConstraidView {
+    /**
+        Constrains the object's leading edge to be greater than or equal to the
+        leading edge of `item`
+     
+        - parameter item: The `item` you want to constrain the current object to
+        - parameter constant: The amount to add to the constraint equation
+          after the multiplier.
+        - parameter multiplier: The ratio altering the constraint relative to
+          leading edge of the item prior to the `constant` being applied.
+        - parameter priority: The priority this constraint uses when being
+          evaluated against other constraints
+
+        - returns: Constraint collection containing the generated constraint
+    */
     @discardableResult
     open func expand(fromLeadingEdgeOf item: Any?,
         offsetBy offset: CGFloat = 0.0, multiplier: CGFloat = 1.0,
@@ -22,6 +36,20 @@ extension ConstraidView {
         return collection
     }
 
+    /**
+        Constrains the object's trailing edge to be greater than or equal to the
+        leading edge of `item`
+     
+        - parameter item: The `item` you want to constrain the current object to
+        - parameter constant: The amount to add to the constraint equation
+          after the multiplier.
+        - parameter multiplier: The ratio altering the constraint relative to
+          trailing edge of the item prior to the `constant` being applied.
+        - parameter priority: The priority this constraint uses when being
+          evaluated against other constraints
+
+        - returns: Constraint collection containing the generated constraint
+    */
     @discardableResult
     open func expand(fromTrailingEdgeOf item: Any?,
         offsetBy offset: CGFloat = 0.0,
@@ -40,6 +68,20 @@ extension ConstraidView {
         return collection
     }
 
+    /**
+        Constrains the object's top edge to be greater than or equal to the
+        top edge of `item`
+     
+        - parameter item: The `item` you want to constrain the current object to
+        - parameter constant: The amount to add to the constraint equation
+          after the multiplier.
+        - parameter multiplier: The ratio altering the constraint relative to
+          top edge of the item prior to the `constant` being applied.
+        - parameter priority: The priority this constraint uses when being
+          evaluated against other constraints
+
+        - returns: Constraint collection containing the generated constraint
+    */
     @discardableResult
     open func expand(fromTopEdgeOf item: Any?,
         offsetBy offset: CGFloat = 0.0, multiplier: CGFloat = 1.0,
@@ -56,6 +98,20 @@ extension ConstraidView {
         return collection
     }
 
+    /**
+        Constrains the object's bottom edge to be greater than or equal to the
+        bottom edge of `item`
+     
+        - parameter item: The `item` you want to constrain the current object to
+        - parameter constant: The amount to add to the constraint equation
+          after the multiplier.
+        - parameter multiplier: The ratio altering the constraint relative to
+          bottom edge of the item prior to the `constant` being applied.
+        - parameter priority: The priority this constraint uses when being
+          evaluated against other constraints
+
+        - returns: Constraint collection containing the generated constraint
+    */
     @discardableResult
     open func expand(fromBottomEdgeOf item: Any?,
         offsetBy offset: CGFloat = 0.0,
@@ -74,6 +130,20 @@ extension ConstraidView {
         return collection
     }
 
+    /**
+        Constrains the object's top & bottom edges to be greater than or equal
+        to the top & bottom edge of `item`
+     
+        - parameter item: The `item` you want to constrain the current object to
+        - parameter constant: The amount to add to the constraint equation
+          after the multiplier.
+        - parameter multiplier: The ratio altering the constraint relative to
+          top & bottom edge of the item prior to the `constant` being applied.
+        - parameter priority: The priority this constraint uses when being
+          evaluated against other constraints
+
+        - returns: Constraint collection containing the generated constraint
+    */
     @discardableResult
     open func expand(fromHorizontalEdgesOf item: Any?,
         offsetBy offset: CGFloat = 0.0,
@@ -89,6 +159,20 @@ extension ConstraidView {
         return collection
     }
 
+    /**
+        Constrains the object's leading & trailing edges to be greater than or equal
+        to the leading & trailing edge of `item`
+     
+        - parameter item: The `item` you want to constrain the current object to
+        - parameter constant: The amount to add to the constraint equation
+          after the multiplier.
+        - parameter multiplier: The ratio altering the constraint relative to
+          leading & trailing edge of the item prior to the `constant` being applied.
+        - parameter priority: The priority this constraint uses when being
+          evaluated against other constraints
+
+        - returns: Constraint collection containing the generated constraint
+    */
     @discardableResult
     open func expand(fromVerticalEdgesOf item: Any?,
         offsetBy offset: CGFloat = 0.0, multiplier: CGFloat = 1.0,
@@ -103,6 +187,22 @@ extension ConstraidView {
         return collection
     }
 
+    /**
+        Constrains the object's top, bottom, leading & trailing edges to be
+        greater than or equal to the top, bottom, leading & trailing edge of
+        `item`
+     
+        - parameter item: The `item` you want to constrain the current object to
+        - parameter constant: The amount to add to the constraint equation
+          after the multiplier.
+        - parameter multiplier: The ratio altering the constraint relative to
+          top, bottom, leading & trailing edge of the item prior to the
+          `constant` being applied.
+        - parameter priority: The priority this constraint uses when being
+          evaluated against other constraints
+
+        - returns: Constraint collection containing the generated constraint
+    */
     @discardableResult
     open func expand(fromEdgesOf item: Any?,
         offsetBy offset: CGFloat = 0.0, multiplier: CGFloat = 1.0,
